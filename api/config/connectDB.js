f3c8a9b89722cbd7760e33e995849c2c02225a1d
Vo Van Truong <vvtruong1007@gmail.com>
2023-02-24 17:27:31 +0700

    commit db

--- api/config/connectDB.js
+++ api/config/connectDB.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-    console.log(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MOMGO_PASSWORD}@booking-app.m00mn4t.mongodb.net/booking?retryWrites=true&w=majority`);
 	try {
 		await mongoose.connect(
 			`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@booking-app.m00mn4t.mongodb.net/booking?retryWrites=true&w=majority`
